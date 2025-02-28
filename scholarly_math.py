ed1388f842a82bc02777a6a8bec383975848db99
scholarly-punk <169328705+scholarly-punk@users.noreply.github.com>
2025-02-28 16:52:52 +1100

    Let there be light

---
+++ scholarly_math.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 17:38:56 2025
+
+@author: ooma
+"""
+
+def is_positive(n):
+    return n > 0
+
+def is_negative(n):
+    return n < 0
+
+def is_integer(n):
+    return isinstance(n, int)
+
+def is_positive_integer(n):
+    return is_integer(n) and is_positive(n)
+
+def is_negative_integer(n):
+    return is_integer(n) and is_negative(n)
+
+def is_non_negative(n):
+    return not is_negative(n)
+
+def is_non_positive(n):
+    return not is_positive(n)
+
+def is_non_negative_integer(n):
+    return is_integer(n) and is_non_negative(n)
+
+def is_non_positive_integer(n):
+    return is_integer(n) and is_non_positive(n)
+
+def is_non_zero(n):
+    return not n == 0
+
+def is_non_zero_integer(n):
+    return is_integer(n) and is_non_zero(n)
+
+def sum_1_to(n):
+    assert is_non_negative_integer(n)
+    return (n * (n + 1)) // 2
